e9ef7bba791c6dfc8067ef52fdf8a3407d77aa2f
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2023-09-26 15:58:11 +0100

    a
    a

---
+++ Scraper v2/Scraper v2/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Drawing;
+
+IWebDriver driver = new FirefoxDriver();
+driver.Url = "https://www.leagueofgraphs.com/summoner/euw/m7md333";
+
+IWebElement tierTF = driver.FindElement(By.ClassName("leagueTier"));
+Console.WriteLine(tierTF.Text); 
+
+var masteries = driver.FindElements(By.ClassName("champColumn"));
+foreach (var mastery in masteries)
+{
+    Console.WriteLine("AAA");
+    Console.WriteLine(mastery.get);
+}
